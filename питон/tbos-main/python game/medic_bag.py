2f0dc6123ccde89677c950e50d31c8d1552a687d
Amahasla1 <do.flam1ngo@yandex.ru>
2025-06-18 09:28:23 +0400

    Delete питон/tbos-main/python game directory

--- питон/tbos-main/python game/medic_bag.py
+++
@@ -1,4 +0,0 @@
-class Medic_bag:
-    def __init__(self, canvas, x, y):
-        self.canvas = canvas
-        self.rect = canvas.create_rectangle(x-5, y-5, x+5, y+5, fill = "green", outline= 'grey', width=0,)
--- питон/tbos-main/python game/player.py
+++
@@ -1,7 +0,0 @@
-class Player:
-    def __init__(self, canvas, x, y):
-        self.canvas = canvas
-        self.rect = canvas.create_rectangle(x - 10, y - 10, x + 10, y + 10, fill = "cyan", outline= 'cyan', width=0)
-        self.keys_pressed = set()
-        self.health = 100
-        self.golden_key=0
